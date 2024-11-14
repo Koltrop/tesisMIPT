77144a6107533ed1c1213dddb826268c71d8ca92
castro daniel <koltropsuriel@gmail.com>
2024-11-14 14:28:20 +0300

    add files un surname_files

--- Program.cs
+++ Program.cs
@@ -4,10 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-using PCRE;
-using Superpower;
-using Superpower.Model;
-using Superpower.Parsers;
 using System.IO;
 
 namespace tesis
@@ -16,73 +12,37 @@ namespace tesis
     {
         static void Main(string[] args)
         {
-            // Папка с файлами фамилий
             string folderPath = Path.Combine(
-                Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName,
-                "surname_files");
+               Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName,
+               "surname_files");
+
             // Шаблон для фамилий, поддерживающий UTF-8 символы и знаки
             string namePattern = @"^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$";
 
-            Console.WriteLine("Тестирование фамилий с System.Text.RegularExpressions:");
-            TestWithRegex(folderPath, namePattern, "System.Text.RegularExpressions");
+            //retest: true for tests
+            TestLibrary.RunTests(folderPath, namePattern, retest: false);
 
-            Console.WriteLine("\nТестирование фамилий с PCRE.NET:");
-            TestWithPCRE(folderPath, namePattern);
-
-            Console.WriteLine("\nТестирование фамилий с Superpower:");
-            TestWithSuperpower(folderPath, namePattern);
+            foreach (var filePath in Directory.GetFiles(folderPath, "*.txt"))
+            {
+                var surnames = File.ReadLines(filePath, Encoding.UTF8);
+                var count = 0;
+                Console.WriteLine($"\nФайл: {Path.GetFileName(filePath)}");
+                foreach (var surname in surnames)
+                {
+                    bool isValid = Regex.IsMatch(surname, namePattern);
+                    if (!isValid)
+                    {
+                        Console.WriteLine($"Фамилия: {surname} - Некорректна");
+                        count++;
+                    }
+                }
+                if(count == 0)
+                {
+                    Console.WriteLine($"Все фамиля в {filePath} прошли проверки");
+                }
+            }
 
             Console.ReadLine();
-        }
-
-        // Функция для загрузки фамилий из файла
-        static IEnumerable<string> LoadSurnames(string filePath)
-        {
-            return File.ReadLines(filePath, Encoding.UTF8);
-        }
-
-        // Тестирование с использованием System.Text.RegularExpressions
-        static void TestWithRegex(string folderPath, string pattern, string libraryName)
-        {
-            foreach (var filePath in Directory.GetFiles(folderPath, "*.txt"))
-            {
-                Console.WriteLine($"\nФайл: {Path.GetFileName(filePath)}");
-                foreach (var surname in LoadSurnames(filePath))
-                {
-                    bool isValid = Regex.IsMatch(surname, pattern);
-                    Console.WriteLine($"Фамилия: {surname} - {(isValid ? "Корректна" : "Некорректна")} ({libraryName})");
-                }
-            }
-        }
-
-        // Тестирование с использованием PCRE.NET
-        static void TestWithPCRE(string folderPath, string pattern)
-        {
-            var pcreRegex = new PcreRegex(pattern);
-            foreach (var filePath in Directory.GetFiles(folderPath, "*.txt"))
-            {
-                Console.WriteLine($"\nФайл: {Path.GetFileName(filePath)}");
-                foreach (var surname in LoadSurnames(filePath))
-                {
-                    bool isValid = pcreRegex.IsMatch(surname);
-                    Console.WriteLine($"Фамилия: {surname} - {(isValid ? "Корректна" : "Некорректна")} (PCRE.NET)");
-                }
-            }
-        }
-
-        // Тестирование с использованием Superpower
-        static void TestWithSuperpower(string folderPath, string pattern)
-        {
-            var superpowerPattern = Span.Regex(pattern);
-            foreach (var filePath in Directory.GetFiles(folderPath, "*.txt"))
-            {
-                Console.WriteLine($"\nФайл: {Path.GetFileName(filePath)}");
-                foreach (var surname in LoadSurnames(filePath))
-                {
-                    bool isValid = superpowerPattern.TryParse(surname).HasValue;
-                    Console.WriteLine($"Фамилия: {surname} - {(isValid ? "Корректна" : "Некорректна")} (Superpower)");
-                }
-            }
         }
     }
 }
---
+++ TestLibrary.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using PCRE;
+using Superpower;
+using Superpower.Model;
+using Superpower.Parsers;
+
+namespace tesis
+{
+    public class TestLibrary
+    {
+        public static void RunTests(string folderPath, string namePattern, bool retest = false)
+        {
+            if (retest)
+            {
+                Console.WriteLine("Тестирование фамилий с System.Text.RegularExpressions:");
+                TestWithRegex(folderPath, namePattern);
+
+                Console.WriteLine("\nТестирование фамилий с PCRE.NET:");
+                TestWithPCRE(folderPath, namePattern);
+
+                Console.WriteLine("\nТестирование фамилий с Superpower:");
+                TestWithSuperpower(folderPath, namePattern);
+            }
+        }
+
+        // Функция для загрузки фамилий из файла
+        public static IEnumerable<string> LoadSurnames(string filePath)
+        {
+            return File.ReadLines(filePath, Encoding.UTF8);
+        }
+
+        // Тестирование с использованием System.Text.RegularExpressions
+        public static void TestWithRegex(string folderPath, string pattern)
+        {
+            foreach (var filePath in Directory.GetFiles(folderPath, "*.txt"))
+            {
+                Console.WriteLine($"\nФайл: {Path.GetFileName(filePath)}");
+                foreach (var surname in LoadSurnames(filePath))
+                {
+                    bool isValid = Regex.IsMatch(surname, pattern);
+                    Console.WriteLine($"Фамилия: {surname} - {(isValid ? "Корректна" : "Некорректна")} (System.Text.RegularExpressions)");
+                }
+            }
+        }
+
+        // Тестирование с использованием PCRE.NET
+        public static void TestWithPCRE(string folderPath, string pattern)
+        {
+            var pcreRegex = new PcreRegex(pattern);
+            foreach (var filePath in Directory.GetFiles(folderPath, "*.txt"))
+            {
+                Console.WriteLine($"\nФайл: {Path.GetFileName(filePath)}");
+                foreach (var surname in LoadSurnames(filePath))
+                {
+                    bool isValid = pcreRegex.IsMatch(surname);
+                    Console.WriteLine($"Фамилия: {surname} - {(isValid ? "Корректна" : "Некорректна")} (PCRE.NET)");
+                }
+            }
+        }
+
+        // Тестирование с использованием Superpower
+        public static void TestWithSuperpower(string folderPath, string pattern)
+        {
+            var superpowerPattern = Span.Regex(pattern);
+            foreach (var filePath in Directory.GetFiles(folderPath, "*.txt"))
+            {
+                Console.WriteLine($"\nФайл: {Path.GetFileName(filePath)}");
+                foreach (var surname in LoadSurnames(filePath))
+                {
+                    bool isValid = superpowerPattern.TryParse(surname).HasValue;
+                    Console.WriteLine($"Фамилия: {surname} - {(isValid ? "Корректна" : "Некорректна")} (Superpower)");
+                }
+            }
+        }
+    }
+}
